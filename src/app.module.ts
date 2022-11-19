c9f112c0960198edff2d9fdc6b70807897065c22
xdd <45672942@qq.com>
2022-11-19 15:15:19 +0800

    nest g resource curd 实现接口

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
 import { TestModule } from './test/test.module';
+import { DemoController } from './demo/demo.controller';
+import { DemoModule } from './demo/demo.module';
+import { UserModule } from './user/user.module';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [TestModule],
-  controllers: [AppController, CatsController],
+  imports: [TestModule, DemoModule, UserModule, CatsModule],
+  controllers: [AppController, DemoController],
   providers: [AppService],
 })
 export class AppModule {}
