7eb00ee840f832467fbd086e74168d6a89ad3070
xdd <45672942@qq.com>
2023-02-07 22:01:34 +0800

    1.全局中间件 可以指定一些白名单 2.引入cors解决跨域问题

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -5,9 +5,9 @@ import { Request, Response, NextFunction } from "express";
 @Injectable()
 export class Logger implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('中间件被调用');
-    // 如果不想使用next 可以发送消息
-    res.send('我被拦截了')
-    // next()
+    // console.log('中间件被调用');
+    // // 如果不想使用next 可以发送消息
+    // res.send('我被拦截了')
+    next()
   }
 }
