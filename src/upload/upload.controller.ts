47477b16e03b63354cb741b4b598014edcdd619b
xdd <45672942@qq.com>
2023-02-08 23:46:33 +0800

    对流文件和普通文件下载和上传

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, Res } from '@nestjs/common';
 import { UploadService } from './upload.service';
+import { Response } from 'express';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express/multer';
+import { zip } from 'compressing'; //把zip放大从compressing引入
+import { join } from 'path';
 // FileInterceptor单个文件 
 // FilesInterceptor多个文件 支持批量上传
 
@@ -13,6 +16,29 @@ export class UploadController {
   upload(@UploadedFile() file) {
     console.log('file名称', file)
     return true;
+  }
+  //普通文件
+  @Get('export')
+  downLoad(@Res() res: Response) {
+    console.log('res', res);
+    const url = join(__dirname, '../images/pdf.pdf');
+    res.download(url);
+  }
+  
+  //流文件下载
+  @Get('stream')
+  async down(@Res() res: Response) {
+    console.log('res', res);
+    const url = join(__dirname, '../images/pdf.pdf');
+    const tarStream = new zip.Stream();
+    await tarStream.addEntry(url); //异步的
+    res.setHeader('Content-Type', 'application/octet-steam');
+    res.setHeader(
+      'Content-Disposition',
+      `attachment; filename=fileName`
+    );
+    tarStream.pipe(res);
+
   }
  
 }
