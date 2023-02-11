6439acadae4a050e1aa2e7e86339b580deb6a791
xdd <45672942@qq.com>
2023-02-12 01:14:56 +0800

    登录接口

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,11 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from './config/config.module'
 import { MenuModule } from './menu/menu.module';
 import { UploadModule } from './upload/upload.module';
+import { LoginModule } from './login/login.module';
 @Module({
   imports: [ListModule, UserModule, ConfigModule.forRoot({
     path: '/xiaoxue'
-  }), MenuModule, UploadModule],
+  }), MenuModule, UploadModule, LoginModule],
   controllers: [AppController],
   providers: [AppService],
 })
