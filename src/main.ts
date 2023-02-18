92a629a4f72d8fd79f624d09afed84276e233712
xdd <45672942@qq.com>
2023-02-18 15:47:01 +0800

    异常拦截器

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Response } from './common/response'
+import { Response } from './common/response';
+import { HttpFilter } from './common/filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(new Response())
+  app.useGlobalInterceptors(new Response());
+  app.useGlobalFilters(new HttpFilter());
   await app.listen(3000);
 }
 bootstrap();
